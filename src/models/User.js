e9654e6ebf73d8bfb418232f8f19186a4779d7c7
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-29 16:59:12 +0900

    feat: Add User model

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const ModelBuilder = require('./ModelBuilder');
+
+class User {
+    getId() {
+        return this.id;
+    }
+}
+
+class UserBuilder extends ModelBuilder{
+    constructor(id) {
+        super(User);
+        this.id;
+    }
+}
+module.exports = {
+    User,
+    UserBuilder
+};
