5c3c830ad006da612caf26384832f03ef95c3280
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-31 15:28:40 +0900

    test: Add mock of UserRepo"

---
+++ src/repositories/definitions/__mocks__/UserRepo.js
@@ -0,0 +1,20 @@
+const { UserBuilder } = require('../../../models/User');
+
+let mocking_user_datas;
+const mock = {};
+mock.mockClear = jest.fn().mockImplementation(() => {
+    mocking_user_datas = [ new UserBuilder(5).build() ];
+
+});
+
+mock.findAll = jest.fn()
+    .mockImplementation(() => {
+        return mocking_user_datas;
+    });
+mock.create = jest.fn()
+    .mockImplementation((user) => {
+        const new_user = new UserBuilder(user.getId()).build();
+        mocking_user_datas.push(new_user);
+    });
+
+module.exports = mock;
