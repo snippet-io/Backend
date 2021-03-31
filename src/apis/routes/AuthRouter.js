92ad9cf3ee093f96ce6d322eb2722e7c77e9126f
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-31 16:05:09 +0900

    feat: route /auth/github/accesstoken

--- src/apis/routes/AuthRouter.js
+++ src/apis/routes/AuthRouter.js
@@ -4,6 +4,6 @@ const controller = require('../../controllers/AuthController');
 const router = Router();
 
 router.get('/login', controller.login);
-
+router.get('/accesstoken', controller.issueAccessToken);
 
 module.exports = router;
