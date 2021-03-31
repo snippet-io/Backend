9af27b8e33a6f2214ff27f401bd63c75fbb550a4
Jungwoo-Son <jungwoo9356@gmail.com>
2021-03-31 16:03:42 +0900

    feat: Implement issueAccessToken

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,9 +1,16 @@
 const configs = require('../configs');
+const AuthService = require('../services/AuthService');
 const controllers = {};
 
 controllers.login = async (req, res, next) => {
     const redirect = req.query.redirect;
     res.redirect(`https://github.com/login/oauth/authorize?redirect_uri=${redirect}&client_id=${configs.GITHUB_CLIENT_ID}`);
+};
+
+controllers.issueAccessToken = async (req, res, next) => {
+    const code = req.query.code;
+    const access_token = await AuthService.createAccessToken(code);
+    res.send(access_token);
 };
 
 module.exports = controllers;
