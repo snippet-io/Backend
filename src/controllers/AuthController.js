ca3e3aa408bb073700a5acf40326357942c91cf0
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-02 21:28:34 +0900

    fix: Change method approaching login page

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,7 +4,7 @@ const controllers = {};
 
 controllers.login = async (req, res, next) => {
     const redirect = req.query.redirect;
-    res.redirect(`https://github.com/login/oauth/authorize?redirect_uri=${redirect}&client_id=${configs.GITHUB_CLIENT_ID}`);
+    res.send(`https://github.com/login/oauth/authorize?redirect_uri=${redirect}&client_id=${configs.GITHUB_CLIENT_ID}`);
 };
 
 controllers.issueAccessToken = async (req, res, next) => {
