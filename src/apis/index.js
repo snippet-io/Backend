9e1a619a03743732b2cb84b6f9be2f78e8fce040
Jungwoo Son <44115353+Jungwoo-Son@users.noreply.github.com>
2021-04-02 17:51:40 +0900

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,6 +3,8 @@ const AuthRouter = require('./routes/AuthRouter');
 const api = Router();
 
 api.use('/auth/github', AuthRouter);
-
+api.get('/simple', (req, res) => {
+  res.send(3000);
+});
 
 module.exports = api;
