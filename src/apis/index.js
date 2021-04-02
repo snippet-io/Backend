29d99d99cf46bac945f7a5cedb80747ef78d145f
Jungwoo Son <44115353+Jungwoo-Son@users.noreply.github.com>
2021-04-02 18:23:34 +0900

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,5 +6,8 @@ api.use('/auth/github', AuthRouter);
 api.get('/simple', (req, res) => {
   res.send('3000');
 });
+api.post('/simple', (req, res) => {
+  res.send('5000');
+});
 
 module.exports = api;
