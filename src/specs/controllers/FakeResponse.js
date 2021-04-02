dd02569c9b67c6ab3d26b4ef7e40d5b4c9af306b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-02 21:34:06 +0900

    fix: Support object not have toJSON function

--- src/specs/controllers/FakeResponse.js
+++ src/specs/controllers/FakeResponse.js
@@ -3,11 +3,14 @@ class FakeResponse {
         this.result = {};
     }
     send(object) {
-        if(object instanceof Array) {
+        if (object.toJSON instanceof Function) {
+            this.result = object.toJSON();
+        }
+        else if(object instanceof Array) {
             this.result = object.map((o) => o.toJSON());
         }
-        else if(object) {
-            this.result = object.toJSON();
+        else {
+            this.result = object;
         }
     }
     redirect(url) {
