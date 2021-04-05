7f533b4440bb9112aea41418a70f21ebb8361bd0
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-05 13:22:00 +0900

    test: Add CodeRepo mock

---
+++ src/repositories/definitions/__mocks__/CodeRepo.js
@@ -0,0 +1,34 @@
+const { CodeBuilder } = require('../../../models/Code');
+
+let mocking_code_datas;
+const mock = {};
+mock.mockClear = jest.fn().mockImplementation(() => {
+    mocking_code_datas = [ new CodeBuilder('코드제목', 'rust', 1).setContent('내용').setId(1).setDescription('설명').build() ];
+    
+});
+mock.repo = {
+    hasMany: jest.fn(),
+    belongsTo: jest.fn()
+};
+
+mock.findAll = jest.fn()
+    .mockImplementation(() => {
+        return mocking_code_datas.map((code) => {
+            code.content = undefined;
+            code.description = undefined;
+            return code;
+        });
+    });
+mock.findById = jest.fn()
+    .mo
+mock.create = jest.fn()
+    .mockImplementation((code) => {
+        const new_code = new CodeBuilder(code.getTitle(), code.getLanguage(), code.getAuthorId())
+            .setContent(code.getContent())
+            .setDescription(code.getDescription())
+            .setId(2)
+            .build();
+        mocking_code_datas.push(new_code);
+    });
+
+module.exports = mock;
