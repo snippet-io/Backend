c785aec3d0f1d080e7425e5132e9cb943383d8b3
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-05 18:49:02 +0900

    feat: Support middleware

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -5,20 +5,50 @@ class Router {
     constructor() {
         this.express_router = express.Router();
     }
-    get(path, callback) {
-        this.express_router.get(path, make_express_controller(callback));
+    get(path, middlewares, callback) {
+        if (!callback) {
+            callback = middlewares;
+            this.express_router.get(path, make_express_controller(callback));
+        }
+        else {
+            this.express_router.get(path, middlewares, make_express_controller(callback));
+        }
     }
-    post(path, callback) {
-        this.express_router.post(path, make_express_controller(callback));
+    post(path, middlewares, callback) {
+        if (!callback) {
+            callback = middlewares;
+            this.express_router.post(path, make_express_controller(callback));
+        }
+        else {
+            this.express_router.post(path, middlewares, make_express_controller(callback));
+        }
     }
-    patch(path, callback) {
-        this.express_router.patch(path, make_express_controller(callback));
+    patch(path, middlewares, callback) {
+        if (!callback) {
+            callback = middlewares;
+            this.express_router.patch(path, make_express_controller(callback));
+        }
+        else {
+            this.express_router.patch(path, middlewares, make_express_controller(callback));
+        }
     }
-    put(path, callback) {
-        this.express_router.put(path, make_express_controller(callback));
+    put(path, middlewares, callback) {
+        if (!callback) {
+            callback = middlewares;
+            this.express_router.put(path, make_express_controller(callback));
+        }
+        else {
+            this.express_router.put(path, middlewares, make_express_controller(callback));
+        }
     }
-    delete(path, callback) {
-        this.express_router.delete(path, make_express_controller(callback));
+    delete(path, middlewares, callback) {
+        if (!callback) {
+            callback = middlewares;
+            this.express_router.delete(path, make_express_controller(callback));
+        }
+        else {
+            this.express_router.delete(path, middlewares, make_express_controller(callback));
+        }
     }
     getExpressRouter() {
         return this.express_router;
