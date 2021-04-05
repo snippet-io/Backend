0d211990ea5bd6b834788923d7b01891e1c3e643
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-05 13:21:14 +0900

    test: Add CodeService test

---
+++ src/specs/services/CodeService.spec.js
@@ -0,0 +1,24 @@
+require('../util').mockAllRepo();
+
+const { CodeBuilder } = require("../../models/Code");
+const { UserRepo, CodeRepo } = require("../../repositories");
+const CodeService = require('../../services/CodeService');
+
+
+describe('Code 서비스 단위 테스트', () => {
+    beforeEach(() => {
+        UserRepo.mockClear();
+        CodeRepo.mockClear();
+    });
+    it('코드 생성 성공', async () => {
+        const new_code = new CodeBuilder('title', 'language', 1).setContent('content').build();
+        await CodeService.createCode(new_code);
+
+        const codes = await CodeRepo.findAll();
+        new_code.content = undefined;
+        expect(codes.map((code) => {
+            code.id = null;
+            return code;
+        })).toContainEqual(new_code);
+    });
+});
