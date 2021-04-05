74013d21b397e349a37b1cda849c666b108fb1d4
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-05 18:49:55 +0900

    feat: Route post /code

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
+const { AccessTokenExtractor} = require('./middlewares');
 const AuthRouter = require('./routes/AuthRouter');
+const CodeRouter = require('./routes/CodeRouter');
 const api = Router();
 
+
 api.use('/auth/github', AuthRouter.getExpressRouter());
+api.use('/code', [AccessTokenExtractor], CodeRouter.getExpressRouter());
 api.get('/simple', (req, res) => {
   res.send('3000');
 });
