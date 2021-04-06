93d28cd8cbbc8b9ab4f3d8b81aea6158b554c208
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 10:13:33 +0900

    refactor: Remove useless logic

--- src/specs/FakeResponse.js
+++ src/specs/FakeResponse.js
@@ -1,20 +1,9 @@
 class FakeResponse {
-    constructor() {
-        this.result = {};
+    status(code) {
+        this.code = code;
     }
-    send(object) {
-        if (object.toJSON instanceof Function) {
-            this.result = object.toJSON();
-        }
-        else if(object instanceof Array) {
-            this.result = object.map((o) => o.toJSON());
-        }
-        else {
-            this.result = object;
-        }
-    }
-    redirect(url) {
-        this.result = url;
+    getStatus() {
+        return this.code;
     }
 }
 
