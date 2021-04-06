49ab8b1845e1ba1ef0f935c1b5ed40ad7756ec9b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-06 09:57:43 +0900

    fix: Throw exception when token is invalid

--- src/apis/middlewares/index.js
+++ src/apis/middlewares/index.js
@@ -1,9 +1,19 @@
 const { AccessToken } = require("../../authentication");
+const { Forbidden, Unauthorized }= require('../../errors/HttpException');
 
 async function AccessTokenExtractor(req, res) {
     const stringfied_token = req.get('Authorization');
 
-    const access_token = await AccessToken.fromString(stringfied_token);
+    if(!stringfied_token) {
+        throw new Unauthorized;
+    }
+
+    let access_token;
+    try {
+        access_token = await AccessToken.fromString(stringfied_token);
+    } catch (e) {
+        throw new Forbidden;
+    }
     req.auth = access_token;
 };
 
