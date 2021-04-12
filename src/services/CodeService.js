6ca5e8dfb1bc39249a69d1ecbbb3c7e59b10f1e8
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-12 15:48:51 +0900

    feat: implement deleteCode

--- src/services/CodeService.js
+++ src/services/CodeService.js
@@ -5,6 +5,9 @@ class CodeService {
     static async createCode(code) {
         await CodeRepo.create(code);
     }
+    static async deleteCode(code_id) {
+        await CodeRepo.delete(code_id);
+    }
 }
 
 module.exports = CodeService;
