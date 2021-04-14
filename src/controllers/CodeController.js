45b5360a4f72d4c5c4441fc0c9ced670c08c7b0d
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-14 09:08:01 +0900

    feat: Implement modifyCode

--- src/controllers/CodeController.js
+++ src/controllers/CodeController.js
@@ -22,5 +22,18 @@ controllers.deleteCode = async (req) => {
 
     await CodeService.deleteCode(code_id);
 };
+controllers.modifyCode = async (req) => {
+    const access_token = req.auth;
+    const code_id = req.params.id;
+    const code_data = req.body;
+
+    const modified_code = new CodeBuilder(code_data.title, code_data.language, access_token.getUserId())
+        .setContent(code_data.content)
+        .setDescription(code_data.description)
+        .setId(code_id)
+        .build();
+
+    await CodeService.modifyCode(modified_code);
+};
 
 module.exports = controllers;
