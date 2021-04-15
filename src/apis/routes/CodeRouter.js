72939753bb6fc95505ae00839c2592b24085ae73
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-15 16:46:05 +0900

    feat: Route modify code

--- src/apis/routes/CodeRouter.js
+++ src/apis/routes/CodeRouter.js
@@ -1,10 +1,12 @@
 const Router = require("./");
 const controller = require('../../controllers/CodeController');
 const { AccessTokenExtractor } = require('../middlewares');
+const controllers = require("../../controllers/CodeController");
 
 const router = new Router();
 
 router.post('/', [AccessTokenExtractor], controller.createCode);
 router.delete('/:id', [AccessTokenExtractor], controller.deleteCode);
+router.put('/:id', [AccessTokenExtractor], controllers.modifyCode);
 
 module.exports = router;
