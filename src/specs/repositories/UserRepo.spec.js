73a4ad3b76a924e8dd1bf15ae79e392c7a79d996
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 10:30:46 +0900

    test: Test of new pattern

--- src/specs/repositories/UserRepo.spec.js
+++ src/specs/repositories/UserRepo.spec.js
@@ -1,6 +1,7 @@
 const { UserRepo } = require('../../repositories');
 const { UserBuilder } = require('../../models/User');
 const { sequelize } = require('../../loaders/database');
+const UserQueryBuilder = require('../../querybuilders/User');
 
 const simple_user_table = [new UserBuilder(1).build()];
 
@@ -16,13 +17,16 @@ describe('User Repo 통합 테스트', () => {
         await sequelize.close();
     });
     it('findAll 성공 케이스', async () => {
-        const users = await UserRepo.findAll();
+        // const users = await UserRepo.findAll();
+        const users = await new UserQueryBuilder().findAll().excute(transaction);
         expect(users).toEqual(simple_user_table);
     });
     it('create 성공 케이스', async () => {
         const new_user = new UserBuilder(7).build();
-        await UserRepo.create(new_user, transaction);
-        const users = await UserRepo.findAll(transaction);
+        await new UserQueryBuilder().create(new_user).excute(transaction);
+        // await UserRepo.create(new_user, transaction);
+        const users = await new UserQueryBuilder().findAll().excute(transaction);
+        // const users = await UserRepo.findAll(transaction);
         expect(users).toContainEqual(new_user);
     });
 });
