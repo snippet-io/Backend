1d2e59093946fbf367d551a4561f9e570a9a03ca
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 10:40:43 +0900

    test: Test of new pattern '

--- src/specs/services/AuthService.spec.js
+++ src/specs/services/AuthService.spec.js
@@ -1,5 +1,6 @@
 jest.mock('../../external/GithubApp');
 jest.mock('../../configs');
+jest.mock('../../querybuilders/User');
 require('../util').mockAllRepo();
 process.env.TOKEN_SECRET = 'token_secret';
 jest.spyOn(Date, 'now').mockImplementation(() => 1616466983480);
@@ -11,6 +12,7 @@ const { UserBuilder } = require("../../models/User");
 const GithubApp = require("../../external/GithubApp");
 const GithubAppException = require("../../errors/GithubAppException");
 const HttpException = require('../../errors/HttpException');
+const UserQueryBuilder = require('../../querybuilders/User');
 
 const sample_stringified_access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3NfdG9rZW4iLCJ1c2VyX2lkIjo1LCJvYXV0aF90b2tlbiI6Im1vY2tlZF9vYXV0aF90b2tlbiIsImlhdCI6MTYxNjQ2Njk4MywiZXhwIjoxNjE3MDcxNzgzfQ.nYIxhfwLvVTu1fNI4RMdvsdiLapX0Lh26fLh50pqCFU';
 const sample_stringified_access_token_for_unregistered_user = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3NfdG9rZW4iLCJ1c2VyX2lkIjo4MTY1OCwib2F1dGhfdG9rZW4iOiJtb2NrZWRfb2F1dGhfdG9rZW4iLCJpYXQiOjE2MTY0NjY5ODMsImV4cCI6MTYxNzA3MTc4M30.RmbIW4SFcrf08bfZhN2bjHUrcrwB5Y0aRFtZTcFL1Xg';
@@ -19,6 +21,7 @@ describe('AuthService 단위 테스트', () => {
     describe('createAccessToken', () => {
         beforeEach(() => {
             UserRepo.mockClear();
+            UserQueryBuilder.mockClear();
             GithubApp.mockClear();
         });
         it('등록된 유저 엑세스 토큰 발급 성공', async () => {
@@ -36,7 +39,8 @@ describe('AuthService 단위 테스트', () => {
             const access_token = await AuthService.createAccessToken('code');
             expect(access_token.toString()).toBe(sample_stringified_access_token_for_unregistered_user);
             
-            const users = await UserRepo.findAll();
+            const users = await new UserQueryBuilder().findAll().excute();
+            // const users = await UserRepo.findAll();
             expect(users).toContainEqual(new UserBuilder(mocked_user.id).build());
         });
         it('유효하지 않는 코드', async () => {
