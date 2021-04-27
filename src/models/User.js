84f14df78585e395cd39fa1e8049f5116955c4a1
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 14:53:04 +0900

    feat: Apply to include code

--- src/models/User.js
+++ src/models/User.js
@@ -4,12 +4,16 @@ class User {
     getId() {
         return this.id;
     }
+    addCode(code) {
+        this.codes.push(code);
+    }
 }
 
 class UserBuilder extends ModelBuilder{
     constructor(id) {
         super(User);
         this.id = id;
+        this.codes = [];
     }
 }
 module.exports = {
