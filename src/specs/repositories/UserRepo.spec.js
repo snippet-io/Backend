121be813a2b09eae8edfcca775a8aef83f105c8b
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 14:50:56 +0900

    test: Test of includeCode

--- src/specs/repositories/UserRepo.spec.js
+++ src/specs/repositories/UserRepo.spec.js
@@ -1,6 +1,8 @@
 const { UserBuilder } = require('../../models/User');
 const { sequelize } = require('../../loaders/database');
 const UserQueryBuilder = require('../../querybuilders/User');
+const { CodeBuilder } = require('../../models/Code');
+const ServiceTime = require('../../utils/ServiceTime');
 
 const simple_user_table = [new UserBuilder(1).build()];
 
@@ -31,5 +33,11 @@ describe('User Repo 통합 테스트', () => {
     it('findByPk 성공 케이스', async () => {
         const user = await new UserQueryBuilder().findByPk(1).excute(transaction);
         expect(user).toEqual(simple_user_table[0]);
+    });
+    it('includeCode 성공 케이스', async () => {
+        const user = await new UserQueryBuilder().findByPk(1).includeCode().excute(transaction);
+        const expected_user = new UserBuilder(1).build();
+        expected_user.addCode(new CodeBuilder('코드제목', 'rust', 1).setContent('내용').setId(1).setDescription('설명').setCreatedDatetime(new ServiceTime('2021-04-19T00:00:00Z')).build());
+        expect(user).toEqual(expected_user);
     });
 });
