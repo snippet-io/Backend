71cbb74e7856cb4477604afcfa17f9f3b3c70429
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 14:52:10 +0900

    refactor: Alias relation

--- src/repositories/relations.js
+++ src/repositories/relations.js
@@ -1,7 +1,7 @@
 const UserRepo = require('./definitions/UserRepo');
 const CodeRepo = require('./definitions/CodeRepo');
 
-UserRepo.repo.hasMany(CodeRepo.repo, {sourceKey: 'id', foreignKey: 'author_id'});
+UserRepo.repo.hasMany(CodeRepo.repo, {as: 'codes', sourceKey: 'id', foreignKey: 'author_id'});
 CodeRepo.repo.belongsTo(UserRepo.repo, {sourceKey: 'id', foreignKey: 'author_id'});
 
 module.exports = {
