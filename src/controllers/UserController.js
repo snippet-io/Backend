d9b0a5e47c3d46003f6a7ace3ba84189af6a94bb
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-27 22:37:53 +0900

    feat: Implement getCodesOfUser

---
+++ src/controllers/UserController.js
@@ -0,0 +1,13 @@
+const { BadRequest } = require('../errors/HttpException');
+const UserService = require('../services/UserService');
+const controllers = {};
+
+const Joi = require('joi');
+
+controllers.getCodesOfUser = async (req) => {
+    const user_id = req.params.id;
+
+    return await UserService.getCodesOfUser(user_id);
+};
+
+module.exports = controllers;
