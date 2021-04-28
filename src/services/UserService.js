efffcce190de2628c281d6ef8ae8139275d87aab
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-28 09:58:26 +0900

    feat: Implement getUser

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,5 @@
 const { Forbidden, NotFound } = require("../errors/HttpException");
+const GithubApp = require("../external/GithubApp");
 const UserQueryBuilder = require("../querybuilders/User");
 
 
@@ -7,6 +8,17 @@ class CodeService {
         const query = new UserQueryBuilder();
         const user = query.findByPk(user_id).includeCode().excute();
         return user.codes;
+    }
+    static async getUser(user_id) {
+        const github = new GithubApp();
+        const stored_user = await new UserQueryBuilder().findByPk(user_id).excute();
+        
+        const user_data = await github.getUser(stored_user.getName());
+        return {
+            id: user_data.id,
+            name: user_data.name,
+            profile_image_url: user_data.avatar_url
+        };
     }
 }
 
