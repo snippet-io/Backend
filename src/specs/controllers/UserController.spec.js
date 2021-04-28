dd79e4d64887fdeddc43dd605313ed06dfc90376
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-28 15:42:25 +0900

    test: Test it can filter by language

--- src/specs/controllers/UserController.spec.js
+++ src/specs/controllers/UserController.spec.js
@@ -1,4 +1,5 @@
 jest.mock('../../querybuilders/User');
+jest.mock('../../querybuilders/Code');
 jest.mock('../../external/GithubApp');
 
 const controllers = require('../../controllers/UserController');
@@ -8,15 +9,36 @@ const FakeRequestBuilder = require('../FakeRequest');
 const FakeResponse = require('../FakeResponse');
 const UserQueryBuilder = require('../../querybuilders/User');
 const GithubApp = require("../../external/GithubApp");
+const CodeQueryBuilder = require('../../querybuilders/Code');
 
 describe('CodeController 단위 테스트', () => {
     beforeEach(() => {
         UserQueryBuilder.mockClear();
+        CodeQueryBuilder.mockClear();
     });
     it('getCodesOfUser 테스트', async () => {
         const req = new FakeRequestBuilder().setParams({
             id: 1
         }).build();
+        const result = await controllers.getCodesOfUser(req);
+        expect(result.map(r => r.toJSON())).toEqual([{
+            id: 1,
+            title: '코드제목',
+            language: 'rust',
+            content: '내용',
+            description: '설명',
+            author: 1,
+            created_datetime: '2021-04-19T09:00:00.000+09:00'
+        }]);
+    });
+    it('getCodesOfUser 테스트 - language 특정', async () => {
+        const req = new FakeRequestBuilder()
+            .setParams({
+                id: 1
+            })
+            .setQuery({
+                language: 'rust'
+            }).build();
         const result = await controllers.getCodesOfUser(req);
         expect(result.map(r => r.toJSON())).toEqual([{
             id: 1,
