97a30f860f4f2f6c4fe1e5378cd680e5ad14db1f
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-29 16:59:52 +0900

    feat: Route starCode

--- src/apis/routes/CodeRouter.js
+++ src/apis/routes/CodeRouter.js
@@ -2,6 +2,7 @@ const Router = require("./");
 const controller = require('../../controllers/CodeController');
 const { AccessTokenExtractor } = require('../middlewares');
 const controllers = require("../../controllers/CodeController");
+const staring_controllers = require('../../controllers/StaringController');
 
 const router = new Router();
 
@@ -9,5 +10,6 @@ router.post('/', [AccessTokenExtractor], controller.createCode);
 router.delete('/:id', [AccessTokenExtractor], controller.deleteCode);
 router.put('/:id', [AccessTokenExtractor], controllers.modifyCode);
 router.get('/', controllers.getCodes);
+router.post('/:id/stars', [AccessTokenExtractor], staring_controllers.starCode);
 
 module.exports = router;
