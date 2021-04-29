4b9a18dedab20f7fabac4f9bb454915e6724356c
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-29 23:31:42 +0900

    feat: Implement unstarCode

--- src/controllers/StaringController.js
+++ src/controllers/StaringController.js
@@ -8,5 +8,11 @@ controllers.starCode = async (req) => {
 
     await StaringService.starCode(code_id, user_id);
 };
+controllers.unstarCode = async (req) => {
+    const user_id = req.auth.getUserId();
+    const code_id = req.params.id;
+    
+    await StaringService.unstarCode(code_id, user_id);
+};
 
 module.exports = controllers;
