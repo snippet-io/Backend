e5119771e3c5c359e059d64d8e12106834cd073d
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-29 23:10:26 +0900

    test: Mock delete

--- src/querybuilders/__mocks__/Staring.js
+++ src/querybuilders/__mocks__/Staring.js
@@ -9,6 +9,18 @@ const StaringQueryBuilder = jest.fn()
             db.push(staring);
             return this;
         },
+        delete: function (staring) {
+            this.cb = (list_to_be_deleted) => {
+                db = db.filter(s => list_to_be_deleted.findIndex(d => d.user_id === s.user_id && d.code_id === s.code_id) > 0);
+                return list_to_be_deleted;
+            };
+            const index = db.findIndex(s => staring.user_id === s.user_id && staring.code_id === s.code_id);
+            if(index == -1) {
+                throw new NotFound;
+            }
+            this.result = [db[index]];
+            return this;
+        },
         excute: function () {
             if(this.cb) {
                 return this.cb(this.result).length;
