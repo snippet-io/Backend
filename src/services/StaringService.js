1fb765707d73e45f084357ba2a18f47fe5aa6401
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-30 09:25:48 +0900

    feat: Implement getStaredCodeByUser

--- src/services/StaringService.js
+++ src/services/StaringService.js
@@ -7,6 +7,11 @@ class StaringService {
     static async unstarCode(code_id, user_id) {
         await new StaringQueryBuilder().delete({code_id, user_id}).excute();
     }
+    static async getStaredCodeByUser(user_id) {
+        const starings_of_user = await new StaringQueryBuilder().findAll().filterByUser(user_id).includeStaredCode().excute();
+        const result = starings_of_user.map(staring => staring.stared_code);
+        return result;
+    }
 }
 
 module.exports = StaringService;
