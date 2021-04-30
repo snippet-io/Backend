be510b3f9f36dd8e2514247b668e0309c9353b34
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-30 09:49:32 +0900

    feat: Route getStaredCodeByUser

--- src/apis/routes/UserRouter.js
+++ src/apis/routes/UserRouter.js
@@ -1,10 +1,12 @@
 const Router = require("./");
 const controllers = require('../../controllers/UserController');
+const staring_controllers = require('../../controllers/StaringController');
 const { AccessTokenExtractor } = require('../middlewares');
 
 const router = new Router();
 
 router.get('/:id/codes', [AccessTokenExtractor], controllers.getCodesOfUser);
 router.get('/:id', controllers.getUser);
+router.get('/:id/staring/codes', staring_controllers.getStaredCodeByUser);
 
 module.exports = router;
