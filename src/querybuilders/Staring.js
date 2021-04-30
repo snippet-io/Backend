abebaac0323bd51dc280126c32a613c963b08436
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-30 09:00:39 +0900

    feat: Implement includeStaredCode

--- src/querybuilders/Staring.js
+++ src/querybuilders/Staring.js
@@ -1,8 +1,17 @@
 const QueryBuilderMaker = require("@yeoul/sequelize-querybuilder");
+const { CodeRepo } = require("../repositories");
 const StaringRepo = require("../repositories/definitions/StaringRepo");
 
 class StaringQueryScopes {
     static repo = StaringRepo;
+
+    includeStaredCode() {
+        return {
+            include: [
+                { model: CodeRepo.repo, as: 'stared_code'}
+            ]
+        };
+    }
 }
 
 const StaringQueryBuilder = QueryBuilderMaker.make(StaringQueryScopes);
