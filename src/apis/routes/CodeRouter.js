810495ee950937b6dd789a3b83956b2965abceb7
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-07 16:14:46 +0900

    feat: Route getCode

--- src/apis/routes/CodeRouter.js
+++ src/apis/routes/CodeRouter.js
@@ -1,16 +1,21 @@
 const Router = require("./");
-const controller = require('../../controllers/CodeController');
-const { AccessTokenExtractor } = require('../middlewares');
+const controller = require("../../controllers/CodeController");
+const { AccessTokenExtractor } = require("../middlewares");
 const controllers = require("../../controllers/CodeController");
-const staring_controllers = require('../../controllers/StaringController');
+const staring_controllers = require("../../controllers/StaringController");
 
 const router = new Router();
 
-router.post('/', [AccessTokenExtractor], controller.createCode);
-router.delete('/:id', [AccessTokenExtractor], controller.deleteCode);
-router.put('/:id', [AccessTokenExtractor], controllers.modifyCode);
-router.get('/', controllers.getCodes);
-router.post('/:id/stars', [AccessTokenExtractor], staring_controllers.starCode);
-router.delete('/:id/stars', [AccessTokenExtractor], staring_controllers.unstarCode);
+router.post("/", [AccessTokenExtractor], controller.createCode);
+router.delete("/:id", [AccessTokenExtractor], controller.deleteCode);
+router.put("/:id", [AccessTokenExtractor], controllers.modifyCode);
+router.get("/", controllers.getCodes);
+router.get("/:id", controllers.getCode);
+router.post("/:id/stars", [AccessTokenExtractor], staring_controllers.starCode);
+router.delete(
+  "/:id/stars",
+  [AccessTokenExtractor],
+  staring_controllers.unstarCode
+);
 
 module.exports = router;
