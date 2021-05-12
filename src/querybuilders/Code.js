860f3ac79939e2c009b1d11d720bde23d30fba52
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-12 11:35:12 +0900

    style: Fit to Prettier

--- src/querybuilders/Code.js
+++ src/querybuilders/Code.js
@@ -1,49 +1,48 @@
-const QueryBuilderMaker = require('@yeoul/sequelize-querybuilder');
-const { CodeRepo } = require('../repositories');
-const { Op } = require('sequelize');
+const QueryBuilderMaker = require("@yeoul/sequelize-querybuilder");
+const { CodeRepo } = require("../repositories");
+const { Op } = require("sequelize");
 
 class CodeQueryScopes {
-    static repo = CodeRepo;
+  static repo = CodeRepo;
 
-    filterByLanguage(language) {
-        return {
-            where: {
-                language
-            }
-        };
-    }
-    filterById(id) {
-        return {
-            where: {
-                id
-            }
-        };
-    }
-    filterByAuthorId(author_id) {
-        return {
-            where: {
-                author_id
-            }
-        };
-    }
-    searchOnTitleAndContent(search) {
-        return {
-            where: {
-                [Op.or]: [ 
-                    { title: { [Op.like]: '%'+search+'%'}}, 
-                    { content: { [Op.like]: '%'+search+'%'}}
-                ]
-            }
-        };
-    }
-    paginate(limit, offset) {
-        return {
-            limit,
-            offset
-        };
-    }
+  filterByLanguage(language) {
+    return {
+      where: {
+        language,
+      },
+    };
+  }
+  filterById(id) {
+    return {
+      where: {
+        id,
+      },
+    };
+  }
+  filterByAuthorId(author_id) {
+    return {
+      where: {
+        author_id,
+      },
+    };
+  }
+  searchOnTitleAndContent(search) {
+    return {
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: "%" + search + "%" } },
+          { content: { [Op.like]: "%" + search + "%" } },
+        ],
+      },
+    };
+  }
+  paginate(limit, offset) {
+    return {
+      limit,
+      offset,
+    };
+  }
 }
-
 
 const CodeQueryBuilder = QueryBuilderMaker.make(CodeQueryScopes);
 
