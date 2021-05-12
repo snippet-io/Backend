5ed0cfcdf5be64c78c667f78407e6e54e7aaa332
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-12 11:38:17 +0900

    style: Fix to Prettier

--- src/repositories/relations.js
+++ src/repositories/relations.js
@@ -1,14 +1,28 @@
-const UserRepo = require('./definitions/UserRepo');
-const CodeRepo = require('./definitions/CodeRepo');
-const StaringRepo = require('./definitions/StaringRepo');
+const UserRepo = require("./definitions/UserRepo");
+const CodeRepo = require("./definitions/CodeRepo");
+const StaringRepo = require("./definitions/StaringRepo");
 
-UserRepo.repo.hasMany(CodeRepo.repo, {as: 'codes', sourceKey: 'id', foreignKey: 'author_id'});
-CodeRepo.repo.belongsTo(UserRepo.repo, {sourceKey: 'id', foreignKey: 'author_id'});
+UserRepo.repo.hasMany(CodeRepo.repo, {
+  as: "codes",
+  sourceKey: "id",
+  foreignKey: "author_id",
+});
+CodeRepo.repo.belongsTo(UserRepo.repo, {
+  sourceKey: "id",
+  foreignKey: "author_id",
+});
 
-CodeRepo.repo.hasMany(StaringRepo.repo, {sourceKey: 'id', foreignKey: 'code_id'});
-StaringRepo.repo.belongsTo(CodeRepo.repo, {as: 'stared_code', sourceKey: 'id', foreignKey: 'code_id'});
+CodeRepo.repo.hasMany(StaringRepo.repo, {
+  sourceKey: "id",
+  foreignKey: "code_id",
+});
+StaringRepo.repo.belongsTo(CodeRepo.repo, {
+  as: "stared_code",
+  sourceKey: "id",
+  foreignKey: "code_id",
+});
 
 module.exports = {
-    UserRepo,
-    CodeRepo
+  UserRepo,
+  CodeRepo,
 };
