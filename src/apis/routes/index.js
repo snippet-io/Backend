ef4f7d7d3a1da647eafb6e399b66511fdbd49d67
Jungwoo-Son <jungwoo9356@gmail.com>
2021-05-25 17:12:54 +0900

    feat: Add timer

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -1,61 +1,74 @@
 const express = require("express");
 
-
 class Router {
-    constructor() {
-        this.express_router = express.Router();
-    }
-    get(path, middlewares, callback) {
-        wrapHttpMethod(this.express_router, 'get', path, middlewares, callback);
-    }
-    post(path, middlewares, callback) {
-        wrapHttpMethod(this.express_router, 'post', path, middlewares, callback);
-    }
-    patch(path, middlewares, callback) {
-        wrapHttpMethod(this.express_router, 'patch', path, middlewares, callback);
-    }
-    put(path, middlewares, callback) {
-        wrapHttpMethod(this.express_router, 'put', path, middlewares, callback);
-    }
-    delete(path, middlewares, callback) {
-        wrapHttpMethod(this.express_router, 'delete', path, middlewares, callback);
-    }
-    getExpressRouter() {
-        return this.express_router;
-    }
+  constructor() {
+    this.express_router = express.Router();
+  }
+  get(path, middlewares, callback) {
+    wrapHttpMethod(this.express_router, "get", path, middlewares, callback);
+  }
+  post(path, middlewares, callback) {
+    wrapHttpMethod(this.express_router, "post", path, middlewares, callback);
+  }
+  patch(path, middlewares, callback) {
+    wrapHttpMethod(this.express_router, "patch", path, middlewares, callback);
+  }
+  put(path, middlewares, callback) {
+    wrapHttpMethod(this.express_router, "put", path, middlewares, callback);
+  }
+  delete(path, middlewares, callback) {
+    wrapHttpMethod(this.express_router, "delete", path, middlewares, callback);
+  }
+  getExpressRouter() {
+    return this.express_router;
+  }
 }
 
-function wrapHttpMethod(express_rotuer, http_method, path, middlewares, callback) {
-    if (!callback) {
-        callback = middlewares;
-        express_rotuer[http_method](path, make_express_controller(callback));
-    }
-    else {
-        middlewares = middlewares.map((middleware) => make_express_middleware(middleware));
-        express_rotuer[http_method](path, middlewares, make_express_controller(callback));
-    }
+function wrapHttpMethod(
+  express_rotuer,
+  http_method,
+  path,
+  middlewares,
+  callback
+) {
+  if (!callback) {
+    callback = middlewares;
+    express_rotuer[http_method](path, make_express_controller(callback));
+  } else {
+    middlewares = middlewares.map((middleware) =>
+      make_express_middleware(middleware)
+    );
+    express_rotuer[http_method](
+      path,
+      middlewares,
+      make_express_controller(callback)
+    );
+  }
 }
 function make_express_middleware(callback) {
-    return async (req, res, next) => {
-        try {
-            await callback(req, res);
-        } catch (e) {
-            next(e);
-        }
-        next();
-    };
+  return async (req, res, next) => {
+    try {
+      await callback(req, res);
+    } catch (e) {
+      next(e);
+    }
+    next();
+  };
 }
 function make_express_controller(callback) {
-    return async (req, res, next) => {
-        let result;
-        try {
-            result = await callback(req, res);
-        } catch (error) {
-            next(error);
-            return;
-        }
-        res.send(result);
-    };
+  return async (req, res, next) => {
+    let result;
+    const loggerName = Math.random().toString();
+    try {
+      console.time(loggerName);
+      result = await callback(req, res);
+      console.timeEnd(loggerName);
+    } catch (error) {
+      next(error);
+      return;
+    }
+    res.send(result);
+  };
 }
 
 module.exports = Router;
